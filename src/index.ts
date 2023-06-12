232c41a1b367e3f1e7d53b5a3e34600b539da0c3
Guilherme G. Silva <dev.gsilv@gmail.com>
2023-06-12 17:44:48 -0300

    [feat] add express server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import express from 'express'
 
-console.log('hello world!')
+const server = express()
+
+server.get('/', (req, res) => (
+    res.send("Hello, world!")
+))
+
+server.listen(3333, () => {
+    console.log("Server running on port 3333")
+})
